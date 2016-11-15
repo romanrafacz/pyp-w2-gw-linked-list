57a9b4a8ca256e02bbd30ce21a033c5e7269eba0
Roman Rafacz <romanr32@gmail.com>
2016-11-15 23:43:39 +0000

    gettin there

--- linked_list/interface.py
+++ linked_list/interface.py
@@ -1,3 +1,5 @@
+from node import Node
+
 class AbstractLinkedList(object):
     """
     Abstract class representing the LinkedList inteface you must respect.
@@ -6,18 +8,35 @@ class AbstractLinkedList(object):
     must never be instantiated. It's just a "guide" of which methods
     the LinkedList class should respect.
     """
+    
+    def __init__(self, elements=None):
+        self.elements = elements
+        self.start = None
+        self.end = None
+        
+        if self.elements:
+            for element in self.elements:
+                self.append(element)
+        
 
     def __str__(self):
-        raise NotImplementedError()
+        return "[{}]".format(", ".join(strp(x) for x in self.elements)) if self.elements == None else "[]"
+                
 
     def __len__(self):
-        return len(self)
+        return self.count()
 
     def __iter__(self):
         raise NotImplementedError()
 
     def __getitem__(self, index):
-        raise NotImplementedError()
+        if index >= len(self):
+            raise IndexError
+            
+        wantnode = self.start
+        for i in range(index):
+            wantnode.next
+        return wantnode.elem
 
     def __add__(self, other):
         raise NotImplementedError()
@@ -32,10 +51,28 @@ class AbstractLinkedList(object):
         raise NotImplementedError()
 
     def append(self, element):
-        raise NotImplementedError()
+        new_node = Node(element)
+        #new_node.set_start = self.start
+        #new_node.set_next = self.end
+        #self.start = new_node
+        #self.end=new_node.next
+        if self.start is None:
+            self.start = new_node
+            self.end = new_node
+        else:
+            self.end.next = new_node
+            self.end = new_node
 
     def count(self):
-        return __len__(self)
+        if self.start:
+            counter = 1
+            tempNode = self.start
+            while tempNode.next:
+                counter += 1
+                tempNode = tempNode.next
+            return counter
+        else:
+            return 0
 
     def pop(self, index=None):
         raise NotImplementedError()
